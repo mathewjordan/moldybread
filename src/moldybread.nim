bfcc2692e7c1bb5eb8d0213ece99354842328c1b
Mark Baggett <mbagget1@utk.edu>
2019-12-06 11:21:11 -0500

    Start implementing option parsing.

--- src/moldybread.nim
+++ src/moldybread.nim
@@ -1,7 +1,7 @@
-import streams, strutils, xmltools, yaml/serialization, moldybread/fedora
+import streams, strutils, xmltools, yaml/serialization, moldybread/fedora, argparse
 
 type
-  ConfigSettings* = object
+  ConfigSettings = object
     username: string
     password: string
     base_url: string
@@ -14,8 +14,22 @@ proc read_yaml_config(file_path: string): ConfigSettings =
   file_stream.close()
 
 when isMainModule:
+  var p = newParser("Moldybread"):
+    help("Like whitebread but written in nim.")
+    option("-o", "--operation", help="Specify operation", choices = @["harvest_metadata"])
+    option("-d", "--dsid", help="Specify datastream id.", default="MODS")
+    option("-n", "--namespaceorpid", help="Specify containing namespace or PID.", default="")
+  var argv = commandLineParams()
+  var opts = p.parse(argv)
   var yaml_settings = read_yaml_config("/home/mark/nim_projects/moldybread/config/config.yml")
   let fedora_connection = initFedoraRequest(url=yaml_settings.base_url, auth=(yaml_settings.username, yaml_settings.password))
-  fedora_connection.results = fedora_connection.populate_results("test")
-  let test = fedora_connection.harvest_metadata("MODS")
-  echo test.successes
+  case opts.operation
+  of "harvest_metadata":
+    if opts.namespaceorpid != "":
+      fedora_connection.results = fedora_connection.populate_results(opts.namespaceorpid)
+      let test = fedora_connection.harvest_metadata(opts.dsid)
+      echo test.successes
+    else:
+      echo "Must specify a containing namespace or pid."
+  else:
+    echo "No matching operation."
