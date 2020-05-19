2defae51e607b7d2b046de561133f1c5a49f1c0d
Mat Jordan <mat@utk.edu>
2020-05-19 12:35:01 -0400

    Test zero-padding.

--- src/moldybreadpkg/fedora.nim
+++ src/moldybreadpkg/fedora.nim
@@ -254,7 +254,7 @@ method download_page_with_relationship(this: FedoraRecord, output_directory, boo
     if not existsDir(output_path):
       createDir(output_path)
     notice(fmt"Successfully downloaded page {page_number} of {book_pid} from {this.pid}.")
-    discard this.write_output(fmt"{page_number}{extension}", response.body, output_path)
+    discard this.write_output(fmt"{page_number:6}{extension}", response.body, output_path)
     true
   elif this.retries > 0:
     this.retries -= 1
